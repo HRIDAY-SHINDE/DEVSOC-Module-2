7c1ddf269025458152e347204b7f73e30f10df9b
HRIDAY-SHINDE <84182255+HRIDAY-SHINDE@users.noreply.github.com>
2024-01-24 00:56:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const button=document.getElementById("Fetch")
 
-// var search= username.split('').join('');
-// const token = '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';
+
 button.addEventListener('click', function(){
     var search=document.getElementById("search").value
      alert(search)
     console.log(search)
-    // document.getElementsByClassName("result").innerHTML="HRIDAY"
+   
       
     fetch("https://api.github.com/users/"+search)
     .then((result)=>result.json())
@@ -24,6 +23,9 @@ button.addEventListener('click', function(){
             
       
     })
+    
+    
+    .catch(error => console.error('Error fetching data:', error))
     
 
 })
