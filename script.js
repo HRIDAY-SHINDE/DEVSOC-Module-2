dba5be9fe9fa54fb263c598dec9d423063ac82e9
HRIDAY-SHINDE <84182255+HRIDAY-SHINDE@users.noreply.github.com>
2024-01-23 23:52:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ button.addEventListener('click', function(){
     var search=document.getElementById("search").value
      alert(search)
     console.log(search)
+    // document.getElementsByClassName("result").innerHTML="HRIDAY"
+      
     fetch("https://api.github.com/users/"+search)
     .then((result)=>result.json())
     .then((data)=>
     {
         console.log(data)
+      document.getElementById('result').innerHTML = `
+                    <BR>ID: ${data.id}
+                    <BR>login: ${data.login}
+                    <BR>url:${data.html_url}>
+                    <BR>Public Repos: ${data.public_repos}
+                    <br>Followers:${data.followers}
+                    <br>Following:${data.following}
+                `
+            
+      
     })
+    
 
 })
